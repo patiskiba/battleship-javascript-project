fc90b29bd6af41248f23f603abedfcc1a7d38512
patiskiba <patricia.skiba@mscoding.org>
2025-05-19 14:05:25 -0500

    Working random ship generator
    Black square styling for testing

---
+++ app.js
@@ -0,0 +1,38 @@
+//! Randomly select one of the squares as the battleship location
+
+//? All grid squares
+let gridSquares = document.querySelectorAll(".gridSquare");
+
+let gridArray = ["square0", "square1", "square2", "square3", "square4", "square5", "square6", "square7", "square8"];
+
+
+
+
+
+function getShipIndex() {
+    let shipIndex = Math.floor(Math.random() * 9); //? Between 0 to 8
+    
+    console.log(shipIndex);
+    
+    
+
+    return shipIndex;  
+}
+
+let shipSquare = document.querySelector(`.square${getShipIndex()}`);
+
+shipSquare.classList.add("ship");
+console.log(shipSquare);
+
+
+
+
+
+
+
+
+
+
+
+
+
